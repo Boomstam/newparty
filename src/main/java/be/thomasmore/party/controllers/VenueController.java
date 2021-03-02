6358a96835fcd9f05b85a74e7f9f35b3c96dc081
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-03-02 10:20:23 +0100

    Show/Hide filter

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -15,110 +15,24 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    private final Venue[] venues = {
-            new Venue("De Club", "http://declubinfo"),
-            new Venue("De Loods", "http://deloodsinfo"),
-            new Venue("Zapoi", "http://zapoiinfo"),
-            new Venue("Nekkerhal", "http://nekkerhalinfo")
-    };
-    private String outdoorFilter = "all", indoorFilter = "all", capacityFilter = "all";
+    private final static String show ="show", hide = "hide";
+    private String currentFilter = show;
 
-    @GetMapping("/venuelist")
-    public String venueList(Model model) {
+    @GetMapping({"/venuelist", "venuelist/{filter}"})
+    public String venueList(Model model, @PathVariable(required = false) String filter) {
         Iterable<Venue> venues = venueRepository.findAll();
         model.addAttribute("venues", venues);
-        model.addAttribute("outdoorFilter", outdoorFilter);
-        model.addAttribute("indoorFilter", indoorFilter);
-        model.addAttribute("capacityFilter", capacityFilter);
+        if(filter==null)return "venuelist";
+        flipFilter();
+        model.addAttribute("filter", currentFilter);
         return "venuelist";
     }
 
-    @GetMapping({"/venuedetails", "/venuedetails/{idString}"})
-    public String venueDetails(Model model, @PathVariable(required = false) String idString) {
-        int id = -1;
-        try {
-            id = Integer.parseInt(idString);
-        }
-        catch (NumberFormatException e)
-        {
-        }
-        Optional<Venue> venue = venueRepository.findById(id);
-        if(venue.isPresent()){
-            model.addAttribute("venue", venue.get());
+    private void flipFilter(){
+        if(currentFilter.equals(show)){
+            currentFilter = hide;
         } else {
-            model.addAttribute("venue", null);
+            currentFilter = show;
         }
-        int numVenues = (int)venueRepository.count();
-        model.addAttribute("previd", (id>=1 && id <= numVenues) ? (id>1 ? id-1 : numVenues) : null);
-        model.addAttribute("nextid", (id>=1 && id <= numVenues) ? (id<numVenues ? id+1 : 1) : null);
-        return "venuedetails";
-    }
-
-    @GetMapping({"/venuelist/outdoor", "/venuelist/outdoor/{outdoorFilter}"})
-    public String venueListOutdoorFilter(Model model, @PathVariable(required = false) String outdoorFilter) {
-        Iterable<Venue> venues;
-        if(outdoorFilter == null){
-            outdoorFilter = "all";
-        }
-        if(outdoorFilter.equals("yes")){
-            venues = venueRepository.findByOutdoor(true);
-        } else if(outdoorFilter.equals("no")){
-            venues = venueRepository.findByOutdoor(false);
-        } else {
-            venues = venueRepository.findAll();
-            outdoorFilter = "all";
-        }
-        model.addAttribute("venues", venues);
-        model.addAttribute("outdoorFilter", outdoorFilter);
-        model.addAttribute("indoorFilter", indoorFilter);
-        model.addAttribute("capacityFilter", capacityFilter);
-        this.outdoorFilter = outdoorFilter;
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/indoor", "/venuelist/indoor/{indoorFilter}"})
-    public String venueListIndoorFilter(Model model, @PathVariable(required = false) String indoorFilter) {
-        Iterable<Venue> venues;
-        if(indoorFilter == null){
-            indoorFilter = "all";
-        }
-        if(indoorFilter.equals("yes")){
-            venues = venueRepository.findByIndoor(true);
-        } else if(indoorFilter.equals("no")){
-            venues = venueRepository.findByIndoor(false);
-        } else {
-            venues = venueRepository.findAll();
-            indoorFilter = "all";
-        }
-        model.addAttribute("venues", venues);
-        model.addAttribute("outdoorFilter", outdoorFilter);
-        model.addAttribute("indoorFilter", indoorFilter);
-        model.addAttribute("capacityFilter", capacityFilter);
-        this.indoorFilter = indoorFilter;
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/capacity", "/venuelist/capacity/{capacityFilter}"})
-    public String venueListCapacityFilter(Model model, @PathVariable(required = false) String capacityFilter) {
-        Iterable<Venue> venues;
-        if(capacityFilter == null){
-            capacityFilter = "all";
-        }
-        if(capacityFilter.equals("S")){
-            venues = venueRepository.findByCapacityLessThanEqual(200);
-        } else if(capacityFilter.equals("M")){
-            venues = venueRepository.findByCapacityLessThanEqual(500);
-        } else if(capacityFilter.equals("L")){
-            venues = venueRepository.findByCapacityGreaterThan(500);
-        } else {
-            venues = venueRepository.findAll();
-            capacityFilter = "all";
-        }
-        model.addAttribute("venues", venues);
-        model.addAttribute("outdoorFilter", outdoorFilter);
-        model.addAttribute("indoorFilter", indoorFilter);
-        model.addAttribute("capacityFilter", capacityFilter);
-        this.capacityFilter = capacityFilter;
-        return "venuelist";
     }
 }
