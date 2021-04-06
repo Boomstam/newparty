0c5f03eb09291438e7870ab2beee1e4b14c19008
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-04-06 15:04:28 +0200

    First post method

--- src/main/java/be/thomasmore/party/controllers/AdminController.java
+++ src/main/java/be/thomasmore/party/controllers/AdminController.java
@@ -7,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -27,6 +25,19 @@ public class AdminController {
         Optional<Party> optionalParty = partyRepository.findById(id);
         if (optionalParty.isPresent()) {
             model.addAttribute("party", optionalParty.get());
+        }
+        return "admin/partyedit";
+    }
+
+    @PostMapping("/partyedit/{id}")
+    public String partyEditPost(Model model, @PathVariable int id, @RequestParam String partyName) {
+        logger.info("partyeditpost : " + id + " -- new name: " + partyName);
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            Party party = optionalParty.get();
+            party.setName(partyName);
+            partyRepository.save(party);
+            model.addAttribute("party", party);
         }
         return "admin/partyedit";
     }
